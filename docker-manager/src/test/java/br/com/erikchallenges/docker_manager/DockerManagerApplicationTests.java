bf20a5a527faaebbbe3abaeac3feb99a9169605c
ErikPDN <erikpdn1@gmail.com>
2024-09-21 17:03:36 -0300

    Fazendo desafio de docker manager

---
+++ docker-manager/src/test/java/br/com/erikchallenges/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erikchallenges.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
