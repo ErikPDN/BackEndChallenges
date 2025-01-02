43fedcee6ad6eee9a22fc6fd4a02906b9a863d28
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-01-02 12:07:29 -0300

    deixando apenas desafios java nesse repositorio

--- PicPayChallengerPython/payments/scheamas.py
+++
@@ -1,8 +0,0 @@
-from ninja import ModelSchema
-
-from payments.models import Transactions
-
-class TransactionSchema(ModelSchema):
-    class Meta:
-        model = Transactions
-        exclude = ["id", "date"]
--- PicPayChallengerPython/payments/tasks.py
+++
@@ -1,2 +0,0 @@
-def send_notification(payer, payee, amount):
-    print(f'{payer} enviou um pagamento para {payee} no valor de R$ {amount}')
